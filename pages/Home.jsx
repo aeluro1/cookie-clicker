f67f4a1edf275acc42c540d1c88674da6ccafb7c
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-09-12 20:24:22 -0400

    Added daily quote API

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -7,6 +7,8 @@ import { auth } from "../firebaseAuth";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import Button from "../components/Button";
 import { collection, onSnapshot } from "firebase/firestore";
+import TextTicker from "react-native-text-ticker";
+import axios from "axios";
 
 const styles = StyleSheet.create({
   container: {
@@ -79,6 +81,10 @@ const styles = StyleSheet.create({
   rank: {
     fontWeight: "bold",
     width: 30
+  },
+  quote: {
+    fontSize: 20,
+    paddingHorizontal: 20
   }
 });
 
@@ -86,6 +92,7 @@ export default function Home({ navigation }) {
   const [count, setCount] = useState(0);
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState(null);
+  const [quote, setQuote] = useState("Cookies are delicious!")
   const [btnSize, setBtnSize] = useState({
     width: Dimensions.get("window").width * 0.9,
     height: Dimensions.get("window").width * 0.9
@@ -121,6 +128,17 @@ export default function Home({ navigation }) {
       fetchUsers().catch((error) => console.error(error));
     });
 
+    axios.get("https://api.api-ninjas.com/v1/quotes?category=inspirational", {
+      headers: {
+        "X-Api-Key": "y2skG0Esh6dtsOZUSax8cA==Dm6ipEI64lJzrVMw"
+      }
+    })
+      .then((res) => {
+        const quote = res.data[0];
+        setQuote(`${quote.author} - ${quote.quote}`);
+      })
+      .catch((err) => console.error(err));
+
     const clean = () => {
       unsub();
       unsubSnap();
@@ -144,58 +162,65 @@ export default function Home({ navigation }) {
 
   return (
     <View style={styles.container}>
-        <View style={styles.infoContainer}>
-          <Text style={styles.title}>Score: {count}</Text>
-        </View>
-        <TouchableHighlight
-          onPress={incCount}
-          onPressIn={zoomIn}
-          onPressOut={zoomOut}
-          underlayColor="transparent"
-          activeOpacity={1}
-          style={styles.cookieContainer}
-        >
-          <Image
-            source={cookie}
-            style={[styles.cookie, btnSize]}
-            resizeMode="contain"
-          />
-        </TouchableHighlight>
-        <View style={styles.dataContainer}>
-          {!auth.currentUser ? (
-            <Button
-              title="Login"
-              onPress={() => navigation.navigate("Login")}
-              style={styles.btn}
-            />
-          ) : (
-            <Button
-              title="Logout"
-              onPress={() => signOut(auth).then(() => {setCount(0) && console.log(`Logged out: ${user.email}`)}).catch((err) => {console.error(err.msg)})}
-              style={styles.btn}
-            />
-          )}
+      <TextTicker
+        style={styles.quote}
+        scroll
+        scrollSpeed={30}
+        loop
+        repeatSpacer={0}
+      >{quote}</TextTicker>
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>Score: {count}</Text>
+      </View>
+      <TouchableHighlight
+        onPress={incCount}
+        onPressIn={zoomIn}
+        onPressOut={zoomOut}
+        underlayColor="transparent"
+        activeOpacity={1}
+        style={styles.cookieContainer}
+      >
+        <Image
+          source={cookie}
+          style={[styles.cookie, btnSize]}
+          resizeMode="contain"
+        />
+      </TouchableHighlight>
+      <View style={styles.dataContainer}>
+        {!auth.currentUser ? (
           <Button
-            title="Submit Feedback"
-            onPress={() => navigation.navigate("Feedback")}
+            title="Login"
+            onPress={() => navigation.navigate("Login")}
             style={styles.btn}
           />
-          <View style={styles.scrollViewContainer}>
-            <ScrollView style={styles.scrollView} stickyHeaderIndices={[0]}>
-              <View>
-                <Text style={[styles.text, styles.header]}>Leaderboard</Text>
+        ) : (
+          <Button
+            title="Logout"
+            onPress={() => signOut(auth).then(() => {setCount(0) && console.log(`Logged out: ${user.email}`)}).catch((err) => {console.error(err.msg)})}
+            style={styles.btn}
+          />
+        )}
+        <Button
+          title="Submit Feedback"
+          onPress={() => navigation.navigate("Feedback")}
+          style={styles.btn}
+        />
+        <View style={styles.scrollViewContainer}>
+          <ScrollView style={styles.scrollView} stickyHeaderIndices={[0]}>
+            <View>
+              <Text style={[styles.text, styles.header]}>Leaderboard</Text>
+            </View>
+            {users && users.map((u, idx) => (
+              <View key={u.email} style={styles.boardRow}>
+                <Text style={[styles.text, styles.rank, user?.email === u.email && styles.activeUser]}>{idx + 1}</Text>
+                <Text style={[styles.text, { flex: 1 }, user?.email === u.email && styles.activeUser]}>{u.email}</Text>
+                <Text style={[styles.text, user?.email === u.email && styles.activeUser]}>{u.numCookie}</Text>
               </View>
-              {users && users.map((u, idx) => (
-                <View key={u.email} style={styles.boardRow}>
-                  <Text style={[styles.text, styles.rank, user?.email === u.email && styles.activeUser]}>{idx + 1}</Text>
-                  <Text style={[styles.text, { flex: 1 }, user?.email === u.email && styles.activeUser]}>{u.email}</Text>
-                  <Text style={[styles.text, user?.email === u.email && styles.activeUser]}>{u.numCookie}</Text>
-                </View>
-              ))}
-            </ScrollView>
-          </View>
-          <StatusBar style="auto" />
+            ))}
+          </ScrollView>
         </View>
+        <StatusBar style="auto" />
       </View>
+    </View>
   );
 }
